3b8e710a3c389eec75485f9edb7900d43bd32a91
PHAP <106916730+phapscac@users.noreply.github.com>
2024-10-20 21:29:12 +0700

    p

--- DoAn_NOSQL/PersonalForm.Designer.cs
+++ DoAn_NOSQL/PersonalForm.Designer.cs
@@ -36,8 +36,8 @@
             this.lbGmail = new System.Windows.Forms.Label();
             this.lbPhone = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -111,7 +111,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(64, 315);
+            this.button1.Location = new System.Drawing.Point(12, 315);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(348, 55);
             this.button1.TabIndex = 3;
@@ -119,33 +119,35 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(586, 258);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 35);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Chỉnh ảnh";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(457, 315);
+            this.button3.Location = new System.Drawing.Point(402, 315);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(214, 55);
+            this.button3.Size = new System.Drawing.Size(204, 55);
             this.button3.TabIndex = 4;
             this.button3.Text = "Đổi mật khẩu";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(649, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 55);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Đăng xuất";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // PersonalForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(757, 437);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(868, 452);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
@@ -171,7 +173,7 @@
         private System.Windows.Forms.Label lbGmail;
         private System.Windows.Forms.Label lbPhone;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
